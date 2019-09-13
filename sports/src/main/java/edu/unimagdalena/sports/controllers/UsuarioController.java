5ddb91c7391e78022879cfa30f0b76b6405fd1ba
jjpizarro <jjpizarro@gmail.com>
2019-09-12 20:04:41 -0500

    Agregado el metodo para agregar partidas

--- sports/src/main/java/edu/unimagdalena/sports/controllers/UsuarioController.java
+++ sports/src/main/java/edu/unimagdalena/sports/controllers/UsuarioController.java
@@ -9,7 +9,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import edu.unimagdalena.sports.entities.Partida;
 import edu.unimagdalena.sports.entities.Usuario;
+import edu.unimagdalena.sports.repositories.PartidaRepository;
 import edu.unimagdalena.sports.repositories.UsuarioRespository;
 
 @Controller
@@ -17,6 +19,8 @@ public class UsuarioController {
 	
 	@Autowired
 	private UsuarioRespository usuarioRepositorio;
+	@Autowired
+	private PartidaRepository partidaRepositorio;
 	
 	@GetMapping("/home") 
 	public String index(Model model) {
@@ -27,12 +31,26 @@ public class UsuarioController {
 	public String mostrarFormCrearPartida(Model model) {
 		return "crearpartida";
 	}
+	@PostMapping("/crear-partida")
+	public String crearPartida(@Valid Partida partida, Model model) {
+		Usuario  us = usuarioRepositorio.getOne((long) 1);
+		partida.setCreador(us.getNombre());
+		
+		partidaRepositorio.save(partida);
+		return "listarpastidas";
+		
+		
+	}
+	
+	
 	@GetMapping("/registro")
 	public String mostrarFormRegistro(Model model) {
 		Usuario u = new Usuario(); 
 		model.addAttribute("usuario", u);
 		return "registro";
 	}
+	
+	
 	
 	@PostMapping("/registro")
 	public String registroUsuario(@Valid Usuario usuario, BindingResult result, Model model) {
