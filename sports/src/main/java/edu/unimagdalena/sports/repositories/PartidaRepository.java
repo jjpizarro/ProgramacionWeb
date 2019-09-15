30da2eea116e492f45cfb0f6575aba0841ba49df
jjpizarro <jjpizarro@gmail.com>
2019-09-15 17:42:06 -0500

    se agregaron metodos para la busqueda de información

--- sports/src/main/java/edu/unimagdalena/sports/repositories/PartidaRepository.java
+++ sports/src/main/java/edu/unimagdalena/sports/repositories/PartidaRepository.java
@@ -1,10 +1,12 @@
 package edu.unimagdalena.sports.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import edu.unimagdalena.sports.entities.Partida;
 @Repository
 public interface PartidaRepository extends JpaRepository<Partida, Long> {
-
+	List<Partida> findPartidasByCreador(String creador);
 }
